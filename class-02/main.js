36e1a65071391c818f5e34f3e992ede4efa674f6
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2024-03-30 22:12:25 +0500

    Add files via upload

---
+++ class-02/main.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("hellow world");
+/////////////tsc --init (ts  config file)////////////////////////
+/////////////////////npm init -y (package.json file)/////////////////////////
+////////////////////////npm  i @types/node -D (PACKAGE-lock.json file (install types for node.js))
+////////////////////////(.gitidnore   ......   file)
+///////////////////////(tsc && node main.js)
+let message = "aiman khan";
+console.log(message);
---
+++ class-02/main.ts
@@ -0,0 +1,7 @@
+console.log("hellow world");
+/////////////tsc --init (ts  config file)////////////////////////
+/////////////////////npm init -y (package.json file)/////////////////////////
+////////////////////////npm  i @types/node -D (PACKAGE-lock.json file (install types for node.js))
+////////////////////////(.gitidnore   ......   file)
+let message ="aiman khan";
+console.log(message);
